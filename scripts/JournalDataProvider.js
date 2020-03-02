6aab5e04a68c5262f614972084ecf88a2866807c
Brennen <brennen@Users-MacBook-Pro-3.local>
2020-03-02 15:00:13 -0600

    added java to automate data

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -8,24 +8,28 @@
 
 // This is the original data. Can't Touch This.
 const journal = [
-    {
+    {   
+        id: 1,
         date: "07/24/2025",
         concept: "HTML & CSS",
         entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
         mood: "Ok"
     },
     {
+        id: 2,
         date: "07/25/2025",
         concept: "terminal",
         entry: "learned how to navigate terminal",
         mood: "good"
     },
     {
+        id: 3,
         date: "07/26/2025",
         concept: "github",
         entry: "learned git hub workflow",
         mood: "great"
     },{
+        id: 4,
         date: "07/27/2025",
         concept: "team building",
         entry: "learned how to build a site with a team and use github together",
@@ -38,7 +42,7 @@ const journal = [
     raw data in the format that you want
 */
 export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
+    const sortedByDate = journal.sort( 
         (currentEntry, nextEntry) =>
             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
     )
