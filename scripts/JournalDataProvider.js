e3771eed94938f73784db7a5c527fb778ca994cb
brennen <bmar566@gmail.com>
2020-04-15 19:05:57 -0500

    cleaned up code

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,45 +1,45 @@
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container");
 
 const dispatchStateChangeEvent = () => {
-    const entryStateChangeEvent = new CustomEvent("entryStateChanged")
-    eventHub.dispatchEvent(entryStateChangeEvent)
-}
+  const entryStateChangeEvent = new CustomEvent("entryStateChanged");
+  eventHub.dispatchEvent(entryStateChangeEvent);
+};
 
-let entries = []
+let entries = [];
 
-export const useEntries = () => entries.slice()
+export const useEntries = () => entries.slice();
 
 export const getEntries = () => {
-    return fetch(' http://localhost:3000/entries') // Fetch from the API
-.then(response => response.json())  // Parse as JSON
-.then(parsedEntries => {
-    entries = parsedEntries
-    // What should happen when we finally have the array?
-})
-}
+  return fetch(" http://localhost:3000/entries") // Fetch from the API
+    .then((response) => response.json()) // Parse as JSON
+    .then((parsedEntries) => {
+      entries = parsedEntries;
+      // What should happen when we finally have the array?
+    });
+};
 //deleting notes
-export const deleteEntry = entryId => {
-    return fetch(`http://localhost:3000/entries/${entryId}`, {
-        method: "DELETE"
-    })
-        .then(getEntries)
-        .then(dispatchStateChangeEvent)
-}
-
-export const saveEntries = entry => {
-    return fetch('http://localhost:3000/entries', {
-       method: "Post",
-       headers: {
-           "content-Type": "application/json"
-       },
-       body: JSON.stringify(entry) 
-    })
+export const deleteEntry = (entryId) => {
+  return fetch(`http://localhost:3000/entries/${entryId}`, {
+    method: "DELETE",
+  })
     .then(getEntries)
-    .then(dispatchStateChangeEvent)
-}
-   
+    .then(dispatchStateChangeEvent);
+};
+
+export const saveEntries = (entry) => {
+  return fetch("http://localhost:3000/entries", {
+    method: "Post",
+    headers: {
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify(entry),
+  })
+    .then(getEntries)
+    .then(dispatchStateChangeEvent);
+};
+
 // export const useEntries = () => {
-//     const sortedByDate = entries.sort( 
+//     const sortedByDate = entries.sort(
 //         (currentEntry, nextEntry) =>
 //             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
 //     )
--- scripts/journalEntryList.js
+++ scripts/journalEntryList.js
@@ -1,40 +1,34 @@
-
-
 // DOM reference to where all entries will be rendered
-import { useEntries, getEntries, deleteEntry } from "./JournalDataProvider.js"
-import { JournalEntryComponent } from "./JournalEntry.js"
+import { useEntries, getEntries, deleteEntry } from "./JournalDataProvider.js";
+import { JournalEntryComponent } from "./JournalEntry.js";
 
-const contentTarget = document.querySelector(".entryLog")
-const eventHub =document.querySelector(".container")
+const contentTarget = document.querySelector(".entryLog");
+const eventHub = document.querySelector(".container");
 
-eventHub.addEventListener("entryStateChanged", e =>{
-    render()
-})
+eventHub.addEventListener("entryStateChanged", (e) => {
+  render();
+});
 //deleting note E.V.
-contentTarget.addEventListener("click", e =>{
-    if(e.target.id.startsWith("deleteEntry--")){
-        const [pefix, entryId] = e.target.id.split("--")
-        deleteEntry(entryId)
-    }
-  })
+contentTarget.addEventListener("click", (e) => {
+  if (e.target.id.startsWith("deleteEntry--")) {
+    const [pefix, entryId] = e.target.id.split("--");
+    deleteEntry(entryId);
+  }
+});
 
+const render = () => {
+  getEntries().then(() => {
+    const allTheEntries = useEntries();
 
-
-const render = () =>{
-    getEntries().then(()=>{
-        
-        const allTheEntries = useEntries()
-            
-        contentTarget.innerHTML = allTheEntries.map(
-                currentEntryObject => {
-                    return JournalEntryComponent(currentEntryObject)
-                }
-            ).join(" ")
-    })
-}
+    contentTarget.innerHTML = allTheEntries
+      .map((currentEntryObject) => {
+        return JournalEntryComponent(currentEntryObject);
+      })
+      .join(" ");
+  });
+};
 
 export const EntryListComponent = () => {
-    // Use the journal entry data from the data provider component
-   render()
-    }
-
+  // Use the journal entry data from the data provider component
+  render();
+};
